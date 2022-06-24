37ec9f0ab6df85df3be512386b1d18c9bfcbc644
Katelind Phillips <scarlettkate94@gmail.com>
2022-06-24 15:02:51 -0400

    updated options

--- PalindromeExercise/PalindromeExercise/WordSmith.cs
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 namespace PalindromeExercise
 {
     public class WordSmith
@@ -10,6 +11,27 @@ namespace PalindromeExercise
             char[] c = str.ToCharArray();
             Array.Reverse(c);
             return new string(c).Equals(str);
+
+            //DIFF OPTION
+            //var reversed = "";
+            //for (var i = str.Length - 1; i >=0; i--)
+            //{
+            //    reversed += str.ToLower()[i];
+            //}
+            //if(reversed == str.ToLower())
+            //{
+            //    return true;
+            //}
+            //return false;
+
+            //REFACTOR PREVIOUS
+            //return reversed == str.ToLower();
+
+            //LINQ
+            //return str.ToLower().SequenceEqual(str.ToLower().Reverse());
+
+            //LINQ SENTENCES OPTION
+            //return str.Replace(" ", "").ToLower().SequenceEqual(str.Replace(" ", "").ToLower().Reverse());
         }
     }
 }
